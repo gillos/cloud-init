4b85a9ee04979b4b328ea96d4ead4105d89205dd
Jonas Andersson <ja@kth.se>
2023-05-24 18:15:48 +0200

    fixed new-vm3

--- new-vm3.sh
+++ new-vm3.sh
@@ -3,13 +3,14 @@ source .env
 if [ ! -f "jammy-server-cloudimg-amd64.ova" ];then
    curl -OL https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.ova
 fi
-for i in {26..27};do
+for i in {19..21};do
 govc import.spec jammy-server-cloudimg-amd64.ova > spec.json
 vmname="rancher-node0${i}.cloud.kth.se"
 python3 <<EOF
 import json
 f=open('spec.json')       
 j=json.loads(f.read())
+j['DiskProvisioning']='thin'
 for x in j['PropertyMapping']:
     if x['Key']=='public-keys':
             x['Value']="$SSH_KEY"
@@ -19,12 +20,26 @@ f=open('spec.json','w')
 f.write(json.dumps(j))
 f.close()
 EOF
-govc import.ova -name=${vmname} --options=spec.json jammy-server-cloudimg-amd64.ova
-govc vm.network.add  -vm=${vmname} -net n210 
+govc library.deploy --options=spec.json /ova/jammy-server-cloudimg-amd64 ${vmname}
 govc vm.network.change -vm=${vmname} ethernet-0
 govc vm.change -vm ${vmname} -m 8192 -c 2
 govc vm.disk.change -vm ${vmname} -size 120G 
 MAC=$(govc device.info -vm=${vmname} ethernet-0 | grep "MAC Address" | cut -c 21-37)
-MAC2=$(govc device.info -vm=${vmname} ethernet-1 | grep "MAC Address" | cut -c 21-37)
-echo "${vmname} ${MAC} ${MAC2}"
+IP="130.237.255.$i"
+RES=$(curl -s -X POST -H "apilabel:$KIDDOW_KEY" -H "apisecret:$KIDDOW_SECRET" -d "ipaddress=$IP" -d "macaddress=$MAC" $KIDDOW_URL/api/v0/set_static_ip)
+echo "${vmname} ${MAC}"
+done
+TS=$(date +%s)
+TD=$((TS - 1678217460))
+TM=$(expr $TD % 300)
+TMD=$((300 - TM))
+sleep $TMD
+for i in {19..21};do
+vmname="rancher-node0${i}.cloud.kth.se"
+govc vm.power -on ${vmname}
+done
+sleep 60
+for i in {19..21};do
+vmname="rancher-node0${i}.cloud.kth.se"
+govc vm.network.add  -vm=${vmname} -net v861
 done
